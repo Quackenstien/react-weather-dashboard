57ef4fe854024eadb8692a900c1c5804bf453445
Quackenstien <donaldhawkins422@gmail.com>
2020-07-28 16:18:55 -0700

    changed to form set up state to search by user query

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,34 +1,40 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
 
 function CardForm() {
-  //Set up fetch to call data from weather api
+  //Protected API key
   const API_KEY = process.env.REACT_APP_API_KEY;
 
+  //useState to take city input and build out query
+  const [city, setCity] = useState();
+
+  //Fetch function
   function handleFetchData() {
-    // access in API call
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=phoenix&units=imperial&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`
     )
       .then((res) => res.json())
       .then((data) => console.log(data));
   }
+
   //Need to set up input field to search for specific params
-  //Need to build and set state with retrieved information
 
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button onClick={handleFetchData}>Search</Button>
-      </Card.Body>
-    </Card>
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Control
+          type="text"
+          name="city"
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Search City"
+        />
+      </Form.Group>
+
+      <Button onClick={handleFetchData} variant="primary">
+        Submit
+      </Button>
+    </Form>
   );
 }
 
