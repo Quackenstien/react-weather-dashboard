b9546822c2b2032b4c34900e4cbc3f0c9e93d865
Quackenstien <donaldhawkins422@gmail.com>
2020-07-28 11:53:00 -0700

    fetch built

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,8 +4,15 @@ import { Button } from "react-bootstrap";
 
 function CardForm() {
   //Set up fetch to call data from weather api
-  function handleMe() {
-    console.log("hi");
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  function handleFetchData() {
+    // access in API call
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=phoenix&units=imperial&appid=${API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   }
   //Need to set up input field to search for specific params
   //Need to build and set state with retrieved information
@@ -19,7 +26,7 @@ function CardForm() {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button onClick={handleMe}>Search</Button>
+        <Button onClick={handleFetchData}>Search</Button>
       </Card.Body>
     </Card>
   );
