4af0278e61014224c3a438770e49f5c686d30fdd
Quackenstien <donaldhawkins422@gmail.com>
2020-07-29 04:38:42 -0700

    set up state for the name and temp to populate card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./Components/Header";
-import CardForm from "../src/Components/Card";
+import InputForm from "./Components/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
       <Header />
-      <CardForm />
+      <InputForm />
     </>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,41 +1,56 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 
-function CardForm() {
+function InputForm() {
   //Protected API key
   const API_KEY = process.env.REACT_APP_API_KEY;
 
   //useState to take city input and build out query
   const [city, setCity] = useState();
+  const [name, setName] = useState();
+  const [temp, setTemp] = useState();
 
   //Fetch function
-  function handleFetchData() {
+  const handleFetchData = (e) => {
+    e.preventDefault();
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data));
-  }
+      // .then((data) => console.log(data));
+      // console.log(data.name);
+      .then((data) =>
+        setName(data.name, console.log(data), setTemp(data.main.temp))
+      );
+  };
 
   //Need to set up input field to search for specific params
 
   return (
     <Form>
-      <Form.Group controlId="formBasicEmail">
+      <Form.Group>
         <Form.Control
           type="text"
           name="city"
           onChange={(e) => setCity(e.target.value)}
-          placeholder="Search City"
+          placeholder="City"
         />
       </Form.Group>
 
       <Button onClick={handleFetchData} variant="primary">
-        Submit
+        Search
       </Button>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>City: {name}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+          <Card.Text>Temp: {temp}</Card.Text>
+        </Card.Body>
+      </Card>
     </Form>
   );
 }
 
-export default CardForm;
+export default InputForm;
