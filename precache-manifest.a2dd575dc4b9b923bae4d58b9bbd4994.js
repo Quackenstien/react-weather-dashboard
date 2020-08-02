236b718420c127a68718c84ea9705097c1ee87b2
Quackenstien <donaldhawkins422@gmail.com>
2020-08-02 16:31:04 -0700

    Updates

---
+++ precache-manifest.a2dd575dc4b9b923bae4d58b9bbd4994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa8c0491075e4ba78fbebb56feba6aa6",
+    "url": "/react-weather-dashboard/index.html"
+  },
+  {
+    "revision": "ed13e073a898fd3333d5",
+    "url": "/react-weather-dashboard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b63ea4765494894e7e2d",
+    "url": "/react-weather-dashboard/static/css/main.4775c163.chunk.css"
+  },
+  {
+    "revision": "ed13e073a898fd3333d5",
+    "url": "/react-weather-dashboard/static/js/2.c9238b3c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-weather-dashboard/static/js/2.c9238b3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63ea4765494894e7e2d",
+    "url": "/react-weather-dashboard/static/js/main.fccf1dea.chunk.js"
+  },
+  {
+    "revision": "c001fc078d8d264793a1",
+    "url": "/react-weather-dashboard/static/js/runtime-main.129aa14f.js"
+  }
+]);
